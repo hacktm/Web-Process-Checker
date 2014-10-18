c0196307b9d533ed91ae728da0684f144f57eb25
HBK13 <alex.fronea13@gmail.com>
2014-10-19 00:03:01 +0300

    Adding dynamic process list

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -30,13 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.procBox1 = new System.Windows.Forms.TextBox();
-            this.procBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.startBtn = new System.Windows.Forms.Button();
             this.stopBtn = new System.Windows.Forms.Button();
             this.statusLabel = new System.Windows.Forms.Label();
+            this.processesList = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // notifyIcon
@@ -44,43 +41,11 @@
             this.notifyIcon.Text = "notifyIcon";
             this.notifyIcon.Visible = true;
             // 
-            // procBox1
-            // 
-            this.procBox1.Location = new System.Drawing.Point(97, 12);
-            this.procBox1.Name = "procBox1";
-            this.procBox1.Size = new System.Drawing.Size(248, 27);
-            this.procBox1.TabIndex = 0;
-            // 
-            // procBox2
-            // 
-            this.procBox2.Location = new System.Drawing.Point(97, 45);
-            this.procBox2.Name = "procBox2";
-            this.procBox2.Size = new System.Drawing.Size(248, 27);
-            this.procBox2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(12, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Process #1:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(12, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Process #2:";
-            // 
             // startBtn
             // 
-            this.startBtn.Location = new System.Drawing.Point(16, 78);
+            this.startBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startBtn.Location = new System.Drawing.Point(16, 279);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(157, 30);
             this.startBtn.TabIndex = 4;
@@ -90,8 +55,10 @@
             // 
             // stopBtn
             // 
+            this.stopBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.stopBtn.Enabled = false;
-            this.stopBtn.Location = new System.Drawing.Point(188, 78);
+            this.stopBtn.Location = new System.Drawing.Point(188, 279);
             this.stopBtn.Name = "stopBtn";
             this.stopBtn.Size = new System.Drawing.Size(157, 30);
             this.stopBtn.TabIndex = 5;
@@ -103,26 +70,36 @@
             // 
             this.statusLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.statusLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLabel.Location = new System.Drawing.Point(0, 113);
+            this.statusLabel.Location = new System.Drawing.Point(0, 312);
             this.statusLabel.Name = "statusLabel";
             this.statusLabel.Size = new System.Drawing.Size(357, 24);
             this.statusLabel.TabIndex = 6;
             this.statusLabel.Text = "Not Running";
             this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // processesList
+            // 
+            this.processesList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.processesList.BackColor = System.Drawing.SystemColors.Control;
+            this.processesList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.processesList.Cursor = System.Windows.Forms.Cursors.Default;
+            this.processesList.FormattingEnabled = true;
+            this.processesList.Location = new System.Drawing.Point(16, 12);
+            this.processesList.Name = "processesList";
+            this.processesList.Size = new System.Drawing.Size(329, 266);
+            this.processesList.TabIndex = 7;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(357, 137);
+            this.ClientSize = new System.Drawing.Size(357, 336);
+            this.Controls.Add(this.processesList);
             this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.stopBtn);
             this.Controls.Add(this.startBtn);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.procBox2);
-            this.Controls.Add(this.procBox1);
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -132,19 +109,15 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Process Checker";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.NotifyIcon notifyIcon;
-        private System.Windows.Forms.TextBox procBox1;
-        private System.Windows.Forms.TextBox procBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button startBtn;
         private System.Windows.Forms.Button stopBtn;
         private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.CheckedListBox processesList;
     }
 }
