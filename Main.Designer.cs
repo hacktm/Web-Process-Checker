dcd5a9704762d772fd308e8f1e53120c758f1b5a
Florin Sarba <info@rammbase.com>
2014-10-18 14:18:40 +0300

    Adding C# process checkers and db entries

---
+++ Main.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Process_Checker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.procBox1 = new System.Windows.Forms.TextBox();
+            this.procBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            // 
+            // procBox1
+            // 
+            this.procBox1.Location = new System.Drawing.Point(97, 12);
+            this.procBox1.Name = "procBox1";
+            this.procBox1.Size = new System.Drawing.Size(248, 27);
+            this.procBox1.TabIndex = 0;
+            // 
+            // procBox2
+            // 
+            this.procBox2.Location = new System.Drawing.Point(97, 45);
+            this.procBox2.Name = "procBox2";
+            this.procBox2.Size = new System.Drawing.Size(248, 27);
+            this.procBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Process #1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Process #2:";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(16, 78);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(157, 30);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Enabled = false;
+            this.stopBtn.Location = new System.Drawing.Point(188, 78);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(157, 30);
+            this.stopBtn.TabIndex = 5;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(0, 113);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(357, 24);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Not Running";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(357, 137);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.procBox2);
+            this.Controls.Add(this.procBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.TextBox procBox1;
+        private System.Windows.Forms.TextBox procBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Process_Checker
+{
+    public partial class Main : Form
+    {
+        System.Timers.Timer dbTimer = new System.Timers.Timer();
+
+        System.Timers.Timer cmdTimer = new System.Timers.Timer();
+
+        public Main()
+        {
+            InitializeComponent();
+            dbTimer.Interval = 5000;
+            dbTimer.Elapsed += dbTimer_Elapsed;
+            cmdTimer.Interval = 15000;
+            cmdTimer.Elapsed += cmdTimer_Elapsed;
+        }
+
+        private bool CheckProcName(string procName)
+        {
+            try
+            {
+                Process[] proc = Process.GetProcessesByName(procName);
+                if (proc.Length != 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        private void dbTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Process[] proc1 = Process.GetProcessesByName(procBox1.Text);
+            if(proc1.Length != 0)
+            {
+                Web.GetPost("http://localhost/handlers/update_db.php", "key", "jf9uh4iuhjf0wehfj93", "name", procBox1.Text, "ram", proc1[0].VirtualMemorySize64.ToString(), "peak", proc1[0].PeakVirtualMemorySize64.ToString(),"status", "1");
+            }
+            else
+            {
+                Web.GetPost("http://localhost/handlers/update_db.php", "key", "jf9uh4iuhjf0wehfj93", "name", procBox1.Text, "ram", "0", "peak", "0", "status", "0");
+            }
+            Process[] proc2 = Process.GetProcessesByName(procBox2.Text);
+            if (proc2.Length != 0)
+            {
+                Web.GetPost("http://localhost/handlers/update_db.php", "key", "jf9uh4iuhjf0wehfj93", "name", procBox2.Text, "ram", proc2[0].VirtualMemorySize64.ToString(), "peak", proc2[0].PeakVirtualMemorySize64.ToString(), "status", "1");
+            }
+            else
+            {
+                Web.GetPost("http://localhost/handlers/update_db.php", "key", "jf9uh4iuhjf0wehfj93", "name", procBox2.Text, "ram", "0", "peak", "0", "status", "0");
+            }
+        }
+
+        private void cmdTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+        }
+
+        public void StartTimers()
+        {
+            try
+            {
+                dbTimer.Start();
+                cmdTimer.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StopTimers()
+        {
+            try
+            {
+                dbTimer.Stop();
+                cmdTimer.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (!CheckProcName(procBox1.Text))
+            {
+                MessageBox.Show("Process #1 not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!CheckProcName(procBox2.Text))
+            {
+                MessageBox.Show("Process #2 not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            StartTimers();
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+            statusLabel.Text = "Running";
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            StopTimers();
+            startBtn.Enabled = true;
+            stopBtn.Enabled = false;
+            statusLabel.Text = "Not Running";
+            Web.GetPost("http://localhost/handlers/update_db.php", "key", "jf9uh4iuhjf0wehfj93", "name", procBox1.Text, "ram", "0", "peak", "0", "status", "0");
+            Web.GetPost("http://localhost/handlers/update_db.php", "key", "jf9uh4iuhjf0wehfj93", "name", procBox2.Text, "ram", "0", "peak", "0", "status", "0");
+        }
+    }
+}
